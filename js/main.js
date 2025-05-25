d5fc487833b0d9482a50018317681f8662fd7c35
nKaplyukhin <leonchic01@gmail.com>
2025-05-25 12:57:02 +0000

    Deploying to gh-pages from @ nKaplyukhin/himych@b896d1972c25856168ff69ecc2d26c9ae0404260 🚀

--- js/main.js
+++ js/main.js
@@ -2,5 +2,35 @@
 /******/ 	"use strict";
 var __webpack_exports__ = {};
 
+;// CONCATENATED MODULE: ./constants.js
+const BURGER_BUTTON_CLASS = "burger__button"
+const BURGER_CLOSE_BUTTON_CLASS = "burger-menu__close"
+const BURGER_MENU_CLASS = "burger-menu"
+const BURGER_MENU_ACTIVE_CLASS = "burger-menu__active"
+;// CONCATENATED MODULE: ./index.js
+
+
+
+const getElementFromClassname = (className, parent = document) => parent.querySelector(`.${className}`)
+
+
+const handleChangeVisibleBurgerMenu = (e) => {
+    const burgerNemu = getElementFromClassname(BURGER_MENU_CLASS)
+
+    if (!BURGER_MENU_CLASS) {
+        return
+    }
+
+    burgerNemu.classList.toggle(BURGER_MENU_ACTIVE_CLASS)
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const burgerButton = getElementFromClassname(BURGER_BUTTON_CLASS);
+    const closeBurgerButton = getElementFromClassname(BURGER_CLOSE_BUTTON_CLASS);
+
+    burgerButton.addEventListener("click", handleChangeVisibleBurgerMenu)
+    closeBurgerButton.addEventListener("click", handleChangeVisibleBurgerMenu)
+})
 /******/ })()
 ;
+//# sourceMappingURL=main.js.map
