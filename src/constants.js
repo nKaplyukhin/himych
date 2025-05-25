b896d1972c25856168ff69ecc2d26c9ae0404260
nKaplyukhin <nikita-140294@mail.ru>
2025-05-25 19:54:59 +0700

    add mobile version

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const BURGER_BUTTON_CLASS = "burger__button"
+export const BURGER_CLOSE_BUTTON_CLASS = "burger-menu__close"
+export const BURGER_MENU_CLASS = "burger-menu"
+export const BURGER_MENU_ACTIVE_CLASS = "burger-menu__active"
--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+import { BURGER_BUTTON_CLASS, BURGER_CLOSE_BUTTON_CLASS, BURGER_MENU_ACTIVE_CLASS, BURGER_MENU_CLASS } from "./constants";
 import "./styles/styles.scss";
+
+const getElementFromClassname = (className, parent = document) => parent.querySelector(`.${className}`)
+
+
+const handleChangeVisibleBurgerMenu = (e) => {
+    const burgerNemu = getElementFromClassname(BURGER_MENU_CLASS)
+
+    if (!BURGER_MENU_CLASS) {
+        return
+    }
+
+    burgerNemu.classList.toggle(BURGER_MENU_ACTIVE_CLASS)
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const burgerButton = getElementFromClassname(BURGER_BUTTON_CLASS);
+    const closeBurgerButton = getElementFromClassname(BURGER_CLOSE_BUTTON_CLASS);
+
+    burgerButton.addEventListener("click", handleChangeVisibleBurgerMenu)
+    closeBurgerButton.addEventListener("click", handleChangeVisibleBurgerMenu)
+})
