b2737dfbb82b80eae0abff4aae7f9d1aa584c764
nKaplyukhin <leonchic01@gmail.com>
2025-07-02 14:36:37 +0000

    Deploying to gh-pages from @ nKaplyukhin/himych@ac2a0c75e30dd7db684cd925520ec011685fe520 🚀

--- js/main.js
+++ js/main.js
@@ -7,11 +7,13 @@ const BURGER_BUTTON_CLASS = "burger__button"
 const BURGER_CLOSE_BUTTON_CLASS = "burger-menu__close"
 const BURGER_MENU_CLASS = "burger-menu"
 const BURGER_MENU_ACTIVE_CLASS = "burger-menu__active"
+const BURGER_MENU_LINK_CLASS = "menu__link"
 ;// CONCATENATED MODULE: ./index.js
 
 
 
 const getElementFromClassname = (className, parent = document) => parent.querySelector(`.${className}`)
+const getElementsFromClassname = (className, parent = document) => parent.querySelectorAll(`.${className}`)
 
 
 const handleChangeVisibleBurgerMenu = (e) => {
@@ -24,12 +26,22 @@ const handleChangeVisibleBurgerMenu = (e) => {
     burgerNemu.classList.toggle(BURGER_MENU_ACTIVE_CLASS)
 }
 
+const handleBurgerLinkClick = () => {
+    const burgerNemu = getElementFromClassname(BURGER_MENU_CLASS)
+    burgerNemu.classList.remove(BURGER_MENU_ACTIVE_CLASS)
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const burgerButton = getElementFromClassname(BURGER_BUTTON_CLASS);
+    const burgerLinks = getElementsFromClassname(BURGER_MENU_LINK_CLASS);
     const closeBurgerButton = getElementFromClassname(BURGER_CLOSE_BUTTON_CLASS);
 
     burgerButton.addEventListener("click", handleChangeVisibleBurgerMenu)
     closeBurgerButton.addEventListener("click", handleChangeVisibleBurgerMenu)
+
+    burgerLinks.forEach(burgerLink => {
+        burgerLink.addEventListener("click", handleBurgerLinkClick)
+    })
 })
 /******/ })()
 ;
